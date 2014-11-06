7894a54d26ec38cfe2596037a56e31846f1fa7aa
Michael Herman <hermanmu@gmail.com>
2014-11-05 17:32:04 -0700

    unit tests

--- src/crweb/tests.py
+++ src/crweb/tests.py
@@ -1,4 +1,5 @@
 import unittest
+import rethinkdb as r
 from flask.ext.testing import TestCase
 from web import app
 
@@ -8,6 +9,26 @@ class BaseTestCase(TestCase):
 
     def create_app(self):
         return app
+
+    def setUp(self):
+        # Establish connection
+        r.connect(host="localhost", port=28015, auth_key="R3di5FTW").repl()
+        # Create datbase and tables
+        r.db_create('test').run()
+        r.db('test').table_create('users').run()
+        # Add user
+        r.table("users").insert([
+            {'username': 'admin'},
+            {'email': 'ad@min.com'},
+            {'password': 'admin'},
+            {'company': 'Admin Company'},
+            {'contact': 'Admin Contact'}
+        ]).run()
+        # Close ection
+
+    def tearDown(self):
+        r.connect(host="localhost", port=28015, auth_key="R3di5FTW").repl()
+        r.db_drop('test').run()
 
 
 class FlaskTestCase(BaseTestCase):
@@ -29,6 +50,16 @@ class UserViewsTests(BaseTestCase):
     def test_login_page_loads(self):
         response = self.client.get('/login')
         self.assertIn('Login', response.data)
+
+    # Ensure login behaves correctly with correct credentials - FAIL
+    def test_correct_login(self):
+        with self.client:
+            response = self.client.post(
+                '/login',
+                data=dict(email="ad@min.com", password="admin"),
+                follow_redirects=True
+            )
+            self.assertIn(b'blah', response.data)
 
 
 if __name__ == '__main__':
